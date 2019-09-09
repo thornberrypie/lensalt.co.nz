610ac31862bcd39c792e2e5ac293706dc810e823
GraemeT <graeme.thornber@tourwriter.com>
2019-09-09 17:00:46 +1200

    Add child theme

---
+++ goldthorn/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'goldthorn_enqueue_styles' );
+function goldthorn_enqueue_styles() {
+ 
+    $parent_style = 'twentysixteen-style';
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
